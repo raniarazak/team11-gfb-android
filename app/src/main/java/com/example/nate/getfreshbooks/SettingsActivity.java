5bc5699f5dc37201badca2a5f6752fb585617b8c
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-06-25 13:32:44 +0800

    added settings toolbar

--- app/src/main/java/com/example/nate/getfreshbooks/SettingsActivity.java
+++ app/src/main/java/com/example/nate/getfreshbooks/SettingsActivity.java
@@ -3,13 +3,29 @@ package com.example.nate.getfreshbooks;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
