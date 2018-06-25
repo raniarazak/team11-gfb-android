b5e1a0a4488c633adbe5363cb76d8569a25584d5
natebakescakes <natebakescakes@gmail.com>
2018-06-25 15:06:40 +0800

    Minor BookDetailsActivity changes

--- app/src/main/java/com/example/nate/getfreshbooks/SettingsActivity.java
+++ app/src/main/java/com/example/nate/getfreshbooks/SettingsActivity.java
@@ -1,10 +1,7 @@
 package com.example.nate.getfreshbooks;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
@@ -18,14 +15,15 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId())
+//        {
+//            case android.R.id.home:
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
