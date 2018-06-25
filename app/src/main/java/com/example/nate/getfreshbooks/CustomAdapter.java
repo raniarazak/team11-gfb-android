a6e923395ae190b771e47c23005e853b79b4f747
hejiashuang2018 <hejiashuang2018@outlook.com>
2018-06-25 12:58:49 +0800

    fixed pictures repeat

--- app/src/main/java/com/example/nate/getfreshbooks/CustomAdapter.java
+++ app/src/main/java/com/example/nate/getfreshbooks/CustomAdapter.java
@@ -26,44 +26,58 @@ public class CustomAdapter extends ArrayAdapter<Book> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
         LayoutInflater inflater = (LayoutInflater) getContext()
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 
         View v = inflater.inflate(resource, null);
 
-        Book book = books.get(position);
-        if(convertView!=null) {
-         v=convertView;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.row, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
-        else{
-            if (book != null) {
-                TextView bookTitleView = v.findViewById(R.id.bookTitleView);
-                bookTitleView.setText(book.get("title").toString());
 
-                TextView authorTitleView = v.findViewById(R.id.bookAuthorView);
-                authorTitleView.setText(book.get("author").toString());
-                ImageView imageView = v.findViewById(R.id.image_cover_thumbnail);
-                new AsyncTask<String, Void, Bitmap>() {
-                    @Override
-                    protected Bitmap doInBackground(String... strings) {
-                        return Book.getPhoto(strings[0]);
-                    }
+        Book book = books.get(position);
 
-                    @Override
-                    protected void onPostExecute(Bitmap bitmap) {
-                        imageView.setImageBitmap(bitmap);
-                    }
-                }.execute(book.get("isbn").toString());
+        //  holder.bookPic.setImageBitmap(null);
+        holder.bookTitle.setText(book.get("title").toString());
+        holder.bookAuthor.setText(book.get("author").toString());
 
-//            imageView.setImageBitmap(Book.getPhoto(book.get("isbn").toString()));
+        //holder.bookPic= v.findViewById(R.id.image_cover_thumbnail);
+        new AsyncTask<String, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(String... strings) {
+                return Book.getPhoto(strings[0]);
             }
 
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                holder.bookPic.setImageBitmap(bitmap);
+            }
+        }.execute(book.get("isbn").toString());
+
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        ImageView bookPic;
+        TextView bookAuthor;
+        TextView bookTitle;
+
+
+        public ViewHolder(View view) {
+            bookPic = (ImageView) view.findViewById(R.id.image_cover_thumbnail);
+            bookAuthor = (TextView) view.findViewById(R.id.bookAuthorView);
+            bookTitle = (TextView) view.findViewById(R.id.bookTitleView);
         }
-
-
-
-        return v;
     }
 
 
 }
+
+
